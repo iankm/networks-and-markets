1ca80af6be48b0ace3634fd91cfe0f0b4766ce13
iankm <ianmukherjee@gmail.com>
2019-09-18 10:09:29 -0400

    Homework 1 Files

---
+++ hw1_files/graph.py
@@ -0,0 +1,13 @@
+# include any code you need for your assignment in this file or in auxiliary
+# files that can be imported here.
+
+# given number of nodes n and probability p, output a random graph 
+# as specified in homework
+def create_graph(n,p):
+    return -1
+
+# given a graph G and nodes i,j, output the length of the shortest
+# path between i and j in G.
+def shortest_path(G,i,j):
+    return -1
+
